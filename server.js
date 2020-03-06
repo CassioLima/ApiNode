f1c67de39a30238aff49cb40ed12936638177a0e
Cassio Cortes Cabra de Lima <cclima@cmcorp.com.br>
2020-03-06 14:38:01 -0300

    implementação do Swagger

--- server.js
+++ server.js
@@ -1,11 +1,3 @@
-// const http = require('http');
-
-// const servidor = http.createServer((req, resp) =>{
-//     resp.end('<h1>Servidor rodando...</h1>');
-// });
-
-// servidor.listen(3000);
-
 const app = require('./src/config/custom-express');
 
 app.listen(3000, ()=> { console.log('Servidor rodando na porta 3000')});
--- src/app/controller/livro-controller.js
+++ src/app/controller/livro-controller.js
@@ -3,12 +3,39 @@ const LivroDao = require('../dao/livro-dao'); //constante tem que ter mesmo nome
 
 module.exports = (app) => {
 
+
+
+
+
     app.get('/', (req, resp) => {
         resp.send(
             '<h1>Servidor express em execução!</h1>'
         );
     });
 
+
+/**
+ * @swagger
+ * /livros:
+ *    get:
+ *      tags:
+ *          - Livros
+ *      summary: Retorna Todos os livros.
+ *      description: This is where you can give some background as to why this route is being created or perhaps reference a ticket number.
+ *      consumes:
+ *        - application/json
+ *      parameters:
+ *        - name: body
+ *          in: body
+ *          schema:
+ *            type: object
+ *            properties:
+ *              flavor:
+ *                type: string
+ *      responses:
+ *        200:
+ *          description: Receive back flavor and flavor Id.
+ */    
     app.get('/livros', (req, res) => {
 
         const dao = new LivroDao();
